e10da34a8ba0ea0645965fb6a400bc97acc8269c
Aristeu Roriz <aristeuroriz@gmail.com>
2015-11-17 02:47:42 -0200

    delete services.js

--- server/services.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
- 
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '885128941576104',
-    secret: '3b74fa95859bc29053580d39261566f6'
-});
