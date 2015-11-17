185d6ca24014aaf566cb3cb9829bc2a7c5d99cd4
aristeuroriz <aristeuroriz@gmail.com>
2015-11-17 16:22:11 -0200

    post box ok

--- collections/posts.js
+++ collections/posts.js
@@ -1,19 +1,25 @@
 Posts = new Mongo.Collection('posts');
 
 Posts.publish = function(message, name, avatar) {
- var params = {
-  message: message,
-  time: new Date(),
-  userId: Meteor.userId(),
-  name: name,
-  avatar: avatar
- };
- this.insert(params);
- //winston.info("Posts.publish: ", params);
+    var params = {
+        message: message,
+        time: new Date(),
+        userId: Meteor.userId(),
+        name: name,
+        avatar: avatar
+    };
+    this.insert(params);
+    //winston.info("Posts.publish: ", params);
 };
 Posts.list = function(userIds) {
- return this.find(
-  {userId: {$in: userIds}},
-  {sort: {time: -1, name: 1}}
- );
+    return this.find({
+        userId: {
+            $in: userIds
+        }
+    }, {
+        sort: {
+            time: -1,
+            name: 1
+        }
+    });
 };
